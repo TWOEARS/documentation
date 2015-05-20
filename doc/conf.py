b8394bd8ecc12121ef61c07472e7393c86b1b0dc
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-05-20 16:21:38 +0200

    Update references for AFE and add figure numbering

--- doc/conf.py
+++ doc/conf.py
@@ -20,18 +20,18 @@ import sphinx_rtd_theme
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('./_extensions'))
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #extensions = ['sphinx.ext.autodoc','sphinx.ext.mathjax','rst2pdf.pdfbuilder']
-extensions = ['sphinx.ext.autodoc','sphinx.ext.mathjax']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.mathjax','numfig']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
