20f689f535fb47e234a332e60717893d1cdb7331
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-07-09 10:38:01 +0200

    Add extra file for acronym definitions

---
+++ doc/acronyms.py
@@ -0,0 +1,7 @@
+# This file defines all acronyms used in the documentation, see
+# http://twoears.aipa.tu-berlin.de/doc/dev/writing-documentation.html#using-acronyms
+rst_epilog = """
+.. |HRTF| replace:: :abbr:`HRTF (head-related transfer function)`
+.. |BRIR| replace:: :abbr:`BRIR (binaural room impulse response)`
+.. |BRS|  replace:: :abbr:`BRS (binaural room scaning)`
+"""
