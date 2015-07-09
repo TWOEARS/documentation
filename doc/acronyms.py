681f633a14eac0adbf8a771f8202c3648e49b386
Thomas Forgue <tforgue@laas.fr>
2015-07-09 11:40:25 +0200

    First (incomplete) version of documentation for the robotic platform (to be reviewed).

--- doc/acronyms.py
+++ doc/acronyms.py
@@ -4,4 +4,10 @@ rst_epilog = """
 .. |HRTF| replace:: :abbr:`HRTF (head-related transfer function)`
 .. |BRIR| replace:: :abbr:`BRIR (binaural room impulse response)`
 .. |BRS|  replace:: :abbr:`BRS (binaural room scaning)`
+.. |ROS| replace:: :abbr:`ROS (Robot Operating System)`
+.. |GenoM3| replace:: :abbr:`GenoM3 (Generator of Modules, 3rd version)`
+.. |BASS| replace:: :abbr:`BASS (Binaural Audio Stream Server)`
+.. |BASC| replace:: :abbr:`BASC (Binaural Audio Stream Client)`
+.. |ALSA| replace:: :abbr:`ALSA (Advanced Linux Sound Architecture)`
+.. |FIFO| replace:: :abbr:`FIFO (First-In, First-Out)`
 """
