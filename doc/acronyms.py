cf48c26c5e6ccf11a590b9a0a6180770540fb768
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-07-13 14:05:03 +0200

    Update experiments section

--- doc/acronyms.py
+++ doc/acronyms.py
@@ -1,13 +1,15 @@
 # This file defines all acronyms used in the documentation, see
 # http://twoears.aipa.tu-berlin.de/doc/dev/writing-documentation.html#using-acronyms
 rst_epilog = """
-.. |HRTF| replace:: :abbr:`HRTF (head-related transfer function)`
-.. |BRIR| replace:: :abbr:`BRIR (binaural room impulse response)`
-.. |BRS|  replace:: :abbr:`BRS (binaural room scaning)`
+.. |HRTF| replace:: :abbr:`HRTF (Head-Related Transfer Function)`
+.. |BRIR| replace:: :abbr:`BRIR (Binaural Room Impulse Response)`
+.. |BRS|  replace:: :abbr:`BRS (Binaural Room Scaning)`
 .. |ROS| replace:: :abbr:`ROS (Robot Operating System)`
 .. |GenoM3| replace:: :abbr:`GenoM3 (Generator of Modules, 3rd version)`
 .. |BASS| replace:: :abbr:`BASS (Binaural Audio Stream Server)`
 .. |BASC| replace:: :abbr:`BASC (Binaural Audio Stream Client)`
 .. |ALSA| replace:: :abbr:`ALSA (Advanced Linux Sound Architecture)`
 .. |FIFO| replace:: :abbr:`FIFO (First-In, First-Out)`
+.. |WFS|  replace:: :abbr:`WFS (Wave Field Synthesis)`
+.. |NFC-HOA| replace:: :abbr:'NFC-HOA (Near-Field Compensated Higher Order Ambisonics)`
 """
