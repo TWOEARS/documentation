4b93c75980d0202ad87542cba79e8a816a65d7e8
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-07-16 14:38:55 +0200

    Update logo section and add favicon

--- doc/conf.py
+++ doc/conf.py
@@ -144,16 +144,16 @@ html_theme_path = ["_theme",sphinx_rtd_theme.get_html_theme_path()]
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = "Back to twoears.eu"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "img/twoears.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "twoears-favicon-16x16.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -193,7 +193,7 @@ html_theme_path = ["_theme",sphinx_rtd_theme.get_html_theme_path()]
 #html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #html_show_copyright = True
