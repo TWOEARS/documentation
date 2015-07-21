e8d7102fc35fb359f9a91bfa39fcc7d65dc751a7
Thomas Forgue <tforgue@laas.fr>
2015-07-21 18:30:34 +0200

    Add acronyms and dictonnary entries for the robotic platform.

--- doc/acronyms.py
+++ doc/acronyms.py
@@ -4,12 +4,18 @@ rst_epilog = """
 .. |HRTF| replace:: :abbr:`HRTF (Head-Related Transfer Function)`
 .. |BRIR| replace:: :abbr:`BRIR (Binaural Room Impulse Response)`
 .. |BRS|  replace:: :abbr:`BRS (Binaural Room Scaning)`
+.. |WFS|  replace:: :abbr:`WFS (Wave Field Synthesis)`
+.. |NFC-HOA| replace:: :abbr:'NFC-HOA (Near-Field Compensated Higher Order Ambisonics)`
 .. |ROS| replace:: :abbr:`ROS (Robot Operating System)`
 .. |GenoM3| replace:: :abbr:`GenoM3 (Generator of Modules, 3rd version)`
+.. |robotpkg| replace:: *robotpkg*
+.. |genomix| replace:: *genomix*
+.. |rosix| replace:: *rosix*
+.. |tcl-genomix| replace:: *tcl-genomix*
+.. |matlab-genomix| replace:: *matlab-genomix*
 .. |BASS| replace:: :abbr:`BASS (Binaural Audio Stream Server)`
 .. |BASC| replace:: :abbr:`BASC (Binaural Audio Stream Client)`
 .. |ALSA| replace:: :abbr:`ALSA (Advanced Linux Sound Architecture)`
 .. |FIFO| replace:: :abbr:`FIFO (First-In, First-Out)`
-.. |WFS|  replace:: :abbr:`WFS (Wave Field Synthesis)`
-.. |NFC-HOA| replace:: :abbr:'NFC-HOA (Near-Field Compensated Higher Order Ambisonics)`
+.. |SLAM| replace:: :abbr:`SLAM (Simultaneous Localisation And Mapping)`
 """
