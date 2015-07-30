beb80f9acac6157218df33176e0d5f8327e5afa3
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-07-30 12:28:31 +0200

    Fix bug in get_version.sh

--- get_version.sh
+++ get_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VERSION="$(git symbolic-ref HEAD 2>/dev/null)" || VERSION="(unnamed branch)"
 VERSION=${VERSION##refs/heads/}
-if [ ${VERSION}=='master' ]; then VERSION=latest; fi
+if [ ${VERSION} == 'master' ]; then VERSION=latest; fi
 echo ${VERSION}
