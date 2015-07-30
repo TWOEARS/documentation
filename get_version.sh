720e27fd32a2993232b9bff033e305300309ae1a
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-07-30 12:47:36 +0200

    Remove \n from get_version.sh output

--- get_version.sh
+++ get_version.sh
@@ -2,4 +2,4 @@
 VERSION="$(git symbolic-ref HEAD 2>/dev/null)" || VERSION="(unnamed branch)"
 VERSION=${VERSION##refs/heads/}
 if [ ${VERSION} == 'master' ]; then VERSION=latest; fi
-echo ${VERSION}
+printf ${VERSION}
