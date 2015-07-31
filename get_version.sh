ce49698d4dc30a8dc2244f94c21052a1f636ca29
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-07-31 09:59:02 +0200

    Removed no longer needed get_version.sh bash script

--- get_version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-VERSION="$(git symbolic-ref HEAD 2>/dev/null)" || VERSION="(unnamed branch)"
-VERSION=${VERSION##refs/heads/}
-if [ ${VERSION} == 'master' ]; then VERSION=latest; fi
-printf ${VERSION}
