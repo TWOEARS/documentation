8c1cc38dcc4a14cd6a14991f92836a0c97771399
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2018-01-05 08:33:02 +0000

    Update to new database location

--- links.py
+++ links.py
@@ -6,7 +6,7 @@ links = """
 .. _Two!Ears Binaural Simulator: https://github.com/TWOEARS/binaural-simulator
 .. _Two!Ears Auditory Front-End: https://github.com/TWOEARS/auditory-front-end
 .. _Two!Ears Blackboard System: https://github.com/TWOEARS/blackboard-system
-.. _Two!Ears Database: https://dev.qu.tu-berlin.de/projects/twoears-database
+.. _Two!Ears Database: https://dev.qu.tu-berlin.de/projects/twoears-getdata
 .. _Two!Ears Robotic Platform: https://github.com/TWOEARS
 .. _Auditory Modeling Toolbox: http://amtoolbox.sf.net
 .. _the latest version: https://github.com/TWOEARS/TwoEars/archive/master.zip
